0ef2ff4787b80e598b1644372206309a69882c1c
Sekson Thongon <invisibile.sek@gmail.com>
2021-01-01 14:59:40 +0700

    add cap letter

---
+++ cap_letter.py
@@ -0,0 +1,11 @@
+k = list(str(input()))
+
+result = []
+for i in range(len(k)):
+    if (i+1) % 2 == 0:
+        temp = k[i].upper()
+    else:
+        temp = k[i].lower()
+    result.append(temp)
+    print('1')
+print(*result,sep='')
